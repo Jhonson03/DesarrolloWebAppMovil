01ef0ad89a26f7371660c8060ac2ad423c69493c
Jhonson03 <ls0303032022@unab.edu.sv>
2025-02-12 18:54:08 -0600

    Datos del equipo de desarrollo

---
+++ Asincronica S04/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Datos de Desarrolladores</title>
+    <link rel="stylesheet" href="css/indexstyle.css">
+</head>
+<body>
+    <h1>Desarrolladores de la Aplicación Cafetería</h1>
+    <ul> 
+        <li>Nombres:</li>
+        <li>MORENA BERALIA GUTIERREZ JÓVEL</li>
+        <li>JOSUE BRYAN HERNÁNDEZ ZELAYA</li>
+        <li>JHONSON HUMBERTO LEIVA SALINAS</li>
+        <li>Rol: Desarrolladores Web</li>
+        <li>Email: equipoMBJ@gmail.com</li>
+    </ul>
+    <a href="principal.php">Ir a la Página Principal</a>
+</body>
+</html>
