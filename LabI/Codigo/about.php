8f69a04f7692a2a47a63d899384be96c9d8f7eea
Jhonson03 <ls0303032022@unab.edu.sv>
2025-02-13 20:09:09 -0600

    Add about

--- LabI/Codigo/about.php
+++ LabI/Codigo/about.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sobre los Desarrolladores</title>
-    <link rel="stylesheet" href="vistas/styles.css">
-    <link rel="stylesheet" href="vistas/styleAbout.css">
+    <link rel="stylesheet" href="Styles/styles.css">
+    <link rel="stylesheet" href="Styles/styleAbout.css">
 </head>
 <body>
     <!-- Menú de navegación -->
     <nav>
         <ul>
-            <li><a href="index.php">Formulario</a></li>
+            <li><a href="usuarios.php">Formulario</a></li>
             <li><a href="mostrarUsuarios.php">Usuarios Registrados</a></li>
             <li><a href="about.php">Sobre los Desarrolladores</a></li>
         </ul>
@@ -21,9 +21,9 @@
         <h1>Sobre los Desarrolladores</h1>
         <p>Este proyecto fue desarrollado por el siguiente equipo:</p>
         <ul>
-            <li><strong>Morena Beralia Gutierrez Jovel</strong> - Desarrolladora</li>
+            <li><strong>Morena Beralia Gutierrez Jovel</strong> - Administradora de Base de Datos</li>
             <li><strong>Josue Bryan Hernandez Zelaya</strong> - Diseñador </li>
-            <li><strong>Jhonson Humnberto Leiva Salinas</strong> - Administrador de Base de Datos</li>
+            <li><strong>Jhonson Humnberto Leiva Salinas</strong> - Desarrollador </li>
         </ul>
         <p>Nos apasiona crear soluciones modernas y funcionales para satisfacer las necesidades de nuestros usuarios.</p>
     </div>
--- LabI/Codigo/index.php
+++
@@ -1,1 +0,0 @@
-<?php include 'usuarios.php'; ?>
